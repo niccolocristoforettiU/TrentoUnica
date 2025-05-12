31f502f8e32918659065d5d09db4e2f17d18cc1b
niccolocristoforettiU <n.cristoforetti@studenti.unitn.it>
2025-05-12 20:59:15 +0200

    Aggiunti frontend e backend - TrentoUnica

---
+++ trentounica-backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+// src/middlewares/authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+exports.authenticate = (req, res, next) => {
+  const authHeader = req.header('Authorization');
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Access denied. No token provided.' });
+  }
+
+  const token = authHeader.split(' ')[1]; // 'Bearer <token>'
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied. Invalid token.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid token' });
+  }
+};
