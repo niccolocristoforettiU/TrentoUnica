009838fb2f615e9414adcfa43e37064c4127ca1b
Luis <luismartin.bugnotti@studenti.unitn.it>
2025-05-13 15:37:27 +0200

    Aggiunge rotte separate per dashboard client e organizer

--- trentounica-backend/src/routes/userRoutes.js
+++ trentounica-backend/src/routes/userRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { authenticate } = require('../middlewares/authMiddleware');
+const { authenticate, authorizeRole } = require('../middlewares/authMiddleware');
 
 // Rotta di registrazione
 router.post('/register', userController.register);
