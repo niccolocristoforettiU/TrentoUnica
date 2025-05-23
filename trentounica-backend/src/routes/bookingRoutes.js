afea51bc1d2e1526228a42db13db7de2e1f5fc40
Luis <luismartin.bugnotti@studenti.unitn.it>
2025-05-23 15:15:07 +0200

    Permette annullamento e riattivazione prenotazioni eventi gratuiti

--- trentounica-backend/src/routes/bookingRoutes.js
+++ trentounica-backend/src/routes/bookingRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { authenticate } = require('../middlewares/authMiddleware');
-const { getTicketForEvent, getClientBookings, createBooking } = require('../controllers/bookingController');
+const { getTicketForEvent, getClientBookings, createBooking, cancelBooking } = require('../controllers/bookingController');
 
 router.get('/ticket/:eventId', authenticate, getTicketForEvent);
 router.get('/client', authenticate, getClientBookings);
 router.post('/', authenticate, createBooking); // 👈 aggiunta questa riga
+router.delete('/:bookingId', authenticate, cancelBooking);
 
 module.exports = router;
