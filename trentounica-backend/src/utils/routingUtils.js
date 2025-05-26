c35368a7ffb70c409a2b8e705d839fb2a9eaed5a
niccolocristoforettiU <n.cristoforetti@studenti.unitn.it>
2025-05-26 23:03:55 +0200

    Migliorie per statistiche Comune di Trento

--- trentounica-backend/src/utils/routingUtils.js
+++ trentounica-backend/src/utils/routingUtils.js
@@ -1,18 +1,12 @@
 const axios = require('axios');
 const { isInZTL } = require('./ztlUtils');
 const { findNearestParking } = require('./parkingUtils');
-const { getNearbyPOIFlows: getPostEventWalkRoutes } = require('./poiUtils');
 const { getRouteSegment } = require('./osrmUtils');
 
-async function getStreetRoute(from, to) {
-  if (!isInZTL(to.lat, to.lon)) {
-    return await getRouteSegment(from, to, 'driving');
-  }
-  const parkingPoint = findNearestParking(to);
-  if (!parkingPoint) return [from, to]; // fallback se non ci sono parcheggi validi
-  const drivingRoute = await getRouteSegment(from, parkingPoint, 'driving');
-  const walkingRoute = await getRouteSegment(parkingPoint, to, 'walking');
-  return [...drivingRoute, ...walkingRoute];
+async function getSplitRoute(from, parking, to) {
+  const drivingRoute = (await getRouteSegment(from, parking, 'driving')).map(p => ({ ...p, mode: 'driving' }));
+  const walkingRoute = (await getRouteSegment(parking, to, 'walking')).map(p => ({ ...p, mode: 'walking' }));
+  return { drivingRoute, walkingRoute };
 }
 
-module.exports = { getStreetRoute, getRouteSegment, getPostEventWalkRoutes };
+module.exports = { getSplitRoute, getRouteSegment };
